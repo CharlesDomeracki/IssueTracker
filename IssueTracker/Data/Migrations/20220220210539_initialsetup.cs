7f58e7b378fe0f69e58246d0ad6c706251bdbe69
domerackimark@gmail.com <Charles.Domeracki@claytonhomes.com>
2022-02-20 18:33:26 -0500

    Source code for Issue Tracker

---
+++ IssueTracker/Data/Migrations/20220220210539_initialsetup.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IssueTracker.Data.Migrations
+{
+    public partial class initialsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Issues",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    JiraStory = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    WikiLink = table.Column<string>(nullable: true),
+                    VideoLink = table.Column<string>(nullable: true),
+                    TechNowLink = table.Column<string>(nullable: true),
+                    Notes = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Issues", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Issues");
+        }
+    }
+}
---
+++ IssueTracker/Models/Issues.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IssueTracker.Models
+{
+    public class Issues
+    {
+        public int Id { get; set; }
+        public string JiraStory { get; set; }
+        public string Description { get; set; }
+        public string WikiLink { get; set; }
+        public string VideoLink { get; set; }
+        public string TechNowLink { get; set; }
+        public string Notes { get; set; }
+        public Issues()
+        {
+
+        }
+    }
+}
